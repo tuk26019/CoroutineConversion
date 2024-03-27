ca06a181b1789528f14c3460676dd6f81d02b108
tuk26019 <tuk26019@temple.edu>
2024-03-27 15:36:20 -0400

    changed it so it can utilize Coroutines

--- app/src/main/java/edu/temple/coroutineconversion/MainActivity.kt
+++ app/src/main/java/edu/temple/coroutineconversion/MainActivity.kt
@@ -6,31 +6,32 @@ import android.os.Handler
 import android.os.Looper
 import android.widget.Button
 import android.widget.ImageView
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
 
-    //TODO (Refactor to replace Thread code with coroutines)
-
     lateinit var cakeImageView: ImageView
-
-    val handler = Handler(Looper.getMainLooper(), Handler.Callback {
-        cakeImageView.alpha = it.what / 100f
-        true
-    })
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         cakeImageView = findViewById(R.id.imageView)
+        val scope = CoroutineScope(Job() + Dispatchers.Default)
 
-        findViewById<Button>(R.id.revealButton).setOnClickListener{
-            Thread{
+        findViewById<Button>(R.id.revealButton).setOnClickListener {
+            scope.launch {
                 repeat(100) {
-                    handler.sendEmptyMessage(it)
-                    Thread.sleep(40)
+                    cakeImageView.alpha = it/100f
+                    delay(40)
+
                 }
-            }.start()
+            }
         }
     }
 }
